88ead87f65894c4081a0c6bd425a28ef0c707bf5
quovanitish <nitish.sharma1@quovantis.com>
2021-11-02 16:40:48 +0530

    Implemented partial Header and refactored app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 const port = 3000;
 const app = express();
 
+// Defining paths
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Weather App",
+    name: "Weather App",
+    title: "Forecast",
   });
 });
 
