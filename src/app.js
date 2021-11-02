563d88ac0277e6240de2b16bf214beb55883ee17
quovanitish <nitish.sharma1@quovantis.com>
2021-11-02 15:26:30 +0530

    Added handlebars templating engine

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
