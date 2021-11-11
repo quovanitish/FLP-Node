55fd8dd84e31e96d2354db0beef8f2f8036d1f4d
quovanitish <nitish.sharma1@quovantis.com>
2021-11-11 14:26:43 +0530

    Error handling middleware

---
+++ middlewares/errorMiddlewares.js
@@ -0,0 +1,11 @@
+const errorHandlerMiddleware = (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+};
+
+module.exports = { errorHandlerMiddleware };
