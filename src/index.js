0e0a05f05ac84b9e29a681199aab84e52cbdecce
quovanitish <nitish.sharma1@quovantis.com>
2021-11-12 11:04:49 +0530

    Updated users and tasks routers

--- src/index.js
+++ src/index.js
@@ -1,105 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// create a new user
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201);
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(400);
-      res.send(error);
-    });
-});
-
-// get all users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
-});
-
-// get user by id
-app.get("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  User.findById(userId)
-    .then((user) => {
-      if (!user) {
-        res.status(404);
-        res.send();
-        return;
-      }
-
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
-});
-
-// create a new task
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201);
-      res.send(task);
-    })
-    .catch((error) => {
-      res.status(400);
-      res.send(error);
-    });
-});
-
-// get all tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
-});
-
-// get task by id
-app.get("/tasks/:id", (req, res) => {
-  const id = req.params.id;
-
-  Task.findById(id)
-    .then((task) => {
-      if (!task) {
-        res.status(404);
-        res.send();
-        return;
-      }
-
-      res.send(task);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port: ${port}`);
