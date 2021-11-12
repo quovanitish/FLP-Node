2251788f7c6f93792e19ecf9a708999593533bef
quovanitish <nitish.sharma1@quovantis.com>
2021-11-12 12:41:59 +0530

    Integrated async/await and update user patch endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,47 +2,67 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.post("/users", (req, res) => {
+router.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201);
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(400);
-      res.send(error);
-    });
+  try {
+    await user.save();
+    res.status(201);
+    res.send(user);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
 });
 
-router.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400);
+    res.send();
+  }
 });
 
-router.get("/users/:id", (req, res) => {
+router.get("/users/:id", async (req, res) => {
   const userId = req.params.id;
-  User.findById(userId)
-    .then((user) => {
-      if (!user) {
-        res.status(404);
-        res.send();
-        return;
-      }
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404);
+      res.send();
+      return;
+    }
 
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500);
-      res.send(error);
-    });
+    res.send(user);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+});
+
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.map((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    res.status(400);
+    res.send({ error: "Invalid updates" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+    updates.map((update) => (user[update] = req.body[update]));
+
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(400);
+    res.send(error);
+  }
 });
 
 module.exports = router;
